6a7d19ada8b2ffac6373be5638b20c9fb2315e48
jituumakanta <jitu.umakanta@gmail.com>
2017-10-09 10:22:56 +0530

    code changes according to the api notification

--- app/src/main/java/com/down/pushnotification/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/down/pushnotification/MyFirebaseInstanceIDService.java
@@ -15,13 +15,16 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
 
     @Override
     public void onTokenRefresh() {
-
+        super.onTokenRefresh();
+        System.out.println("refreshedToken: "+"no");
         //Getting registration token
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
 
         //Displaying token on logcat
         Log.d(TAG, "Refreshed token: " + refreshedToken);
-
+        System.out.println("refreshedToken: "+refreshedToken);
+        //token for lenovoa6000 is: etn389zrwG4:APA91bFT7X8D_GWWfffHUvwETo4oTsEdplIMHHyaPG1LPHkMZLfVq1_tBiU7BGlaC1HHHZBcX-djfj0gqH3imlP0ljh5sl_BHjXR5V8teccF0fyV81askk0JOtD2w5VGcEwc2n1g4Krf
+        //token for samsung tab is: d5OOVVnQq6o:APA91bGIkOTQxbjWeYD3TO_ccAzWHjESA5LXR1nyasSbXTd-IFICUYiQ1dmwa_ytWkq4VXx_8F6w0OFiHn4sO81xTasG3z46-6iQvKMUWS8pxZ1kav3MKWgzdMM_zL5K1NvrGg5-oNP9
     }
 
     private void sendRegistrationToServer(String token) {
--- app/src/main/java/com/down/pushnotification/MyFirebaseMessagingService.java
+++ app/src/main/java/com/down/pushnotification/MyFirebaseMessagingService.java
@@ -9,8 +9,12 @@ import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by Wrap me 09 on 04-09-2017.
@@ -22,14 +26,59 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        //Displaying data in log
-        //It is optional
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+
+        FirebaseMessaging.getInstance().subscribeToTopic("global");
+       //Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
 
         //Calling method to generate notification
-        sendNotification(remoteMessage.getNotification().getBody());
+        //sendNotification(remoteMessage.getNotification().getBody());
+        //sendNotification(remoteMessage.getData().get("title"));
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        try {
+            phraseNotificationMessage(remoteMessage.getData().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
     }
+
+
+    public void phraseNotificationMessage(String responseString) throws JSONException {
+        System.out.println("responseString: "+responseString);
+        String remove=responseString.substring(6);
+       // String remove1=remove.substring(6, remove.length() - 1);
+        System.out.println("remove: "+remove);
+        JSONObject jsonObject = new JSONObject(remove);
+        String title = jsonObject.getString("title");
+        String message = jsonObject.getString("message");
+        String imageUrl = jsonObject.getString("image");
+        System.out.println("title:"+title+"message: "+message+"imageUrl: "+imageUrl);
+        addNotification(title,message);
+    }
+
+
+
+
+
+    private void addNotification(String title, String message) {
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle(title)
+                        .setContentText(message);
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        // Add as notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+    }
+
+
 
     //This method is only generating push notification
     //It is same as we did in earlier posts
@@ -39,7 +88,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
-        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("Firebase Push Notification")
